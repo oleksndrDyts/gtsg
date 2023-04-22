4c6b4d97d9d037991653f67ac3ce1b2d9a6f8f41
SashaDyts <lentrofan@gmail.com>
2023-04-22 12:50:27 +0300

    Update useConnect.js

--- src/hooks/multiplayer/useConnect.js
+++ src/hooks/multiplayer/useConnect.js
@@ -31,10 +31,10 @@ const useConnect = ({
 
   useEffect(() => {
     if (isFirstRender.current) {
-      // const newSocket = io.connect(
-      //   'https://gtsg-io-production.up.railway.app/'
-      // );
-      const newSocket = io.connect('http://localhost:5000');
+      const newSocket = io.connect(
+        'https://gtsg-io-production.up.railway.app/'
+      );
+      // const newSocket = io.connect('http://localhost:5000');
       multiInfo.setWebSocket(newSocket);
     }
     isFirstRender.current = false;
